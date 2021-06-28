8ea12b278939563f183c08138bfd747de4e8ef81
huyvo123 <huyvo2581999@gmail.com>
2021-06-28 14:43:07 +0700

    fixed flake8 error

--- app/core/models.py
+++ app/core/models.py
@@ -1,25 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, \
+    BaseUserManager, PermissionsMixin
 # Create your models here.
 
+
 class UserManager(BaseUserManager):
-    
+
     def create_user(self, email, password=None, **extra_fields):
-        #Creates and saves a new user
+        # Creates and saves a new user
         if not email:
             raise ValueError('Users must have an email address')
-        user = self.model(email= self.normalize_email(email), **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
     def create_superuser(self, email, password):
-        #Create and saves a new super user
+        # Create and saves a new super user
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
@@ -30,9 +32,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    
+
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
-
-
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,31 +5,31 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successfull(self):
-        #Test creating a new user with an email is successfull
+        # Test creating a new user with an email is successfull
         email = 'huyvo2581999@gmail.com'
         password = 'huy12345678'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password,
-        ) 
+            email=email,
+            password=password,
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        #Test the email for a new user is normalized"
+        # Test the email for a new user is normalized"
         email = 'test@GMAIL.COM'
         user = get_user_model().objects.create_user(email, '12321')
 
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
-        #Test creating user with no email raises error
+        # Test creating user with no email raises error
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
-    
+
     def test_create_new_superuser(self):
-        email= 'huyvo2581999@gmail.com'
+        email = 'huyvo2581999@gmail.com'
         password = 'sfsa'
         user = get_user_model().objects.create_superuser(email, password)
 
